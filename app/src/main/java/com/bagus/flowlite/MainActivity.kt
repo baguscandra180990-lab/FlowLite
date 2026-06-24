140a3ed5232d0a4bad4e164458b5d6ec6484a1b0
baguscandra180990-lab <baguscandra180990@gmail.com>
2026-06-25 02:27:21 +0700

    Create MainActivity.kt

---
+++ app/src/main/java/com/bagus/flowlite/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.bagus.flowlite
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val webView: WebView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://labs.google/fx/tools/flow")
+    }
+}
